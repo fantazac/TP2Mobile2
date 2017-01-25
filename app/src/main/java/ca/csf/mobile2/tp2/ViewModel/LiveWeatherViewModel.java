2addb554c4db1bb69c6cf54bdbf6fc87d03b6a5c
Philippe Boutin-Therrien <philboutin@hotmail.com>
2017-01-25 08:33:03 -0500

    Changed databiding version & created ViewModel classes

---
+++ app/src/main/java/ca/csf/mobile2/tp2/ViewModel/LiveWeatherViewModel.java
@@ -0,0 +1,8 @@
+package ca.csf.mobile2.tp2.ViewModel;
+
+import android.databinding.BaseObservable;
+
+public class LiveWeatherViewModel extends BaseObservable {
+
+    
+}
---
+++ app/src/main/java/ca/csf/mobile2/tp2/ViewModel/WeatherForecastBundleViewModel.java
@@ -0,0 +1,8 @@
+package ca.csf.mobile2.tp2.ViewModel;
+
+import android.databinding.BaseObservable;
+
+public class WeatherForecastBundleViewModel extends BaseObservable {
+
+
+}
