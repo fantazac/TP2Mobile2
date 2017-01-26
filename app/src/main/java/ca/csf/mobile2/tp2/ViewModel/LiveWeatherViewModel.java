c95cabd5acfc90ff02b662d24e9a42d7e71e7f97
Philippe Boutin-Therrien <philboutin@hotmail.com>
2017-01-25 19:13:14 -0500

    Implemented databing & databinding list

--- app/src/main/java/ca/csf/mobile2/tp2/ViewModel/LiveWeatherViewModel.java
+++ app/src/main/java/ca/csf/mobile2/tp2/ViewModel/LiveWeatherViewModel.java
@@ -1,8 +1,27 @@
 package ca.csf.mobile2.tp2.ViewModel;
 
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
 
-public class LiveWeatherViewModel extends BaseObservable {
+import ca.csf.mobile2.tp2.BR;
+import ca.csf.mobile2.tp2.meteo.LiveWeather;
 
-    
+public class LiveWeatherViewModel extends BaseObservable implements LiveWeather.WeatherListener {
+
+    private final LiveWeather liveWeather;
+
+    public LiveWeatherViewModel(LiveWeather liveWeather) {
+        this.liveWeather = liveWeather;
+    }
+
+    @Bindable
+    public int getCurrentTemperatureInCelsius() {
+        return liveWeather.getCurrentTemperatureInCelsius();
+    }
+
+    @Override
+    public void onWeatherChanged(LiveWeather eventSource) {
+        //notifyPropertyChanged(BR.);
+    }
 }
--- app/src/main/java/ca/csf/mobile2/tp2/ViewModel/WeatherForecastViewModel.java
+++ app/src/main/java/ca/csf/mobile2/tp2/ViewModel/WeatherForecastViewModel.java
@@ -1,8 +1,38 @@
 package ca.csf.mobile2.tp2.ViewModel;
 
-import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 
-public class WeatherForecastViewModel extends BaseObservable {
+import ca.acodebreak.android.databind.list.DatabindableViewModel;
+import ca.csf.mobile2.tp2.math.MathFunction;
+import ca.csf.mobile2.tp2.meteo.WeatherForecast;
+import ca.csf.mobile2.tp2.meteo.WeatherType;
+import ca.csf.mobile2.tp2.time.UtcDay;
 
+public class WeatherForecastViewModel extends DatabindableViewModel<WeatherForecast> {
 
+    private WeatherForecast weatherForecast;
+
+    public WeatherForecastViewModel(WeatherForecast weatherForecast) {
+        this.weatherForecast = weatherForecast;
+    }
+
+    @Bindable
+    public UtcDay getUtcDay() {
+        return weatherForecast.getUtcDay();
+    }
+
+    @Bindable
+    public WeatherType getWeather() {
+        return weatherForecast.getWeather();
+    }
+
+    @Bindable
+    public MathFunction getTemperatureAccordingToUtcTimeFunction() {
+        return weatherForecast.getTemperatureAccordingToUtcTimeFunction();
+    }
+
+    @Override
+    public WeatherForecast getModelObject() {
+        return weatherForecast;
+    }
 }
