57af2729bfa446d2e6ba5fb262dba2cc1ae999bb
Alexandre <alex>
2017-01-27 08:55:17 -0500

    pierre

--- app/src/main/java/ca/csf/mobile2/tp2/ViewModel/WeatherForecastViewModel.java
+++ app/src/main/java/ca/csf/mobile2/tp2/ViewModel/WeatherForecastViewModel.java
@@ -1,14 +1,24 @@
 package ca.csf.mobile2.tp2.ViewModel;
 
 import android.databinding.Bindable;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import ca.acodebreak.android.databind.list.DatabindableViewModel;
+import ca.csf.mobile2.tp2.activity.MainActivity;
+import ca.csf.mobile2.tp2.activity.MainActivity_;
 import ca.csf.mobile2.tp2.math.MathFunction;
 import ca.csf.mobile2.tp2.meteo.WeatherForecast;
 import ca.csf.mobile2.tp2.meteo.WeatherType;
+import ca.csf.mobile2.tp2.time.TimedUtcTimeProvider;
 import ca.csf.mobile2.tp2.time.UtcDay;
 
 public class WeatherForecastViewModel extends DatabindableViewModel<WeatherForecast> {
+
+    private static final int SECONDS_TO_MILLIS = 1000;
 
     private WeatherForecast weatherForecast;
 
@@ -29,7 +39,55 @@ public class WeatherForecastViewModel extends DatabindableViewModel<WeatherForec
     @Bindable
     public MathFunction getTemperatureAccordingToUtcTimeFunction() {
         return weatherForecast.getTemperatureAccordingToUtcTimeFunction();
-    } 
+    }
+
+    public int[] getMinimumAndMaximumTemperaturesForDay(TimedUtcTimeProvider timedUtcTimeProvider){
+        int minimum = 100;
+        int maximum = -100;
+
+        int temperatureAtUtcTime;
+
+        long utcTimeAtMidnight = timedUtcTimeProvider.getCurrentTimeInSeconds() / 60 / 60 / 24 * 60 * 60 * 24;
+
+        for(int i = 0; i < 24; i++){
+            temperatureAtUtcTime = weatherForecast.getTemperatureAt(utcTimeAtMidnight + i * 3600);
+            if(temperatureAtUtcTime > maximum){
+                maximum = temperatureAtUtcTime;
+            }
+            if(temperatureAtUtcTime < minimum){
+                minimum = temperatureAtUtcTime;
+            }
+        }
+
+        return new int[]{minimum, maximum};
+
+    }
+
+    public String getCurrentTime(TimedUtcTimeProvider timedUtcTimeProvider, TextView dateTextView) {
+
+        Date date = new Date(timedUtcTimeProvider.getCurrentTimeInSeconds() * SECONDS_TO_MILLIS);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss", dateTextView.getTextLocale());
+
+        return simpleDateFormat.format(date);
+    }
+
+    public String getCurrentDay(TimedUtcTimeProvider timedUtcTimeProvider, MainActivity_ mainActivity_) {
+
+        Date date = new Date(timedUtcTimeProvider.getCurrentTimeInSeconds() * SECONDS_TO_MILLIS);
+        java.text.DateFormat dateFormat = DateFormat.getLongDateFormat(mainActivity_);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE");
+        String dayOfTheWeek = simpleDateFormat.format(date);
+        dayOfTheWeek = dayOfTheWeek.substring(0, 1).toUpperCase() + dayOfTheWeek.substring(1);
+
+        String restOfDate = dateFormat.format(date);
+        int indexOfFirstLetterOfMonth = restOfDate.indexOf(' ') + 1;
+        restOfDate = restOfDate.substring(0, indexOfFirstLetterOfMonth) +
+                restOfDate.substring(indexOfFirstLetterOfMonth, indexOfFirstLetterOfMonth+1).toUpperCase() +
+                restOfDate.substring(indexOfFirstLetterOfMonth+1);
+
+        return dayOfTheWeek + ", " + restOfDate;
+    }
 
     @Override
     public WeatherForecast getModelObject() {
